8e5b6656ca42db0ca339f688369a7567f686f9dd
Torquoya-Jackson <tlwilliams92tech@yahoo.com>
2021-06-24 18:56:16 -0700

    adding UserForm

---
+++ app/UserForm.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+
+
+
+function UserForm(props) {
+    // setting state
+    const [user, setUser] = useState([]);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
