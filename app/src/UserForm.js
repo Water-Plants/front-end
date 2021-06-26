84f36b5ff3df6e54920eba530b59b9a4cedb615a
Torquoya-Jackson <tlwilliams92tech@yahoo.com>
2021-06-26 14:36:35 -0700

    updating

--- app/src/UserForm.js
+++ app/src/UserForm.js
@@ -1,23 +1,6 @@
-import axios from 'axios';
-//import { useState, useEffect } from 'react';
-// import { useForm } from 'react-hook-form';
+
 import React from 'react';
-import * as yup from 'yup';
 
-//class form
-// class UserForm extends React.Component{
-//     render() {
-//         return (
-//             <div className="UserForm">
-                
-//         </div>
-//     )
-
-//     }
-// }
-
-
-// export default UserForm;
 
 
 //hook format
