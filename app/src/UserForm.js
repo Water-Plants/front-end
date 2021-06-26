79d95bd32a9aacdb3a24c38e5af1a9d18abe5514
Torquoya-Jackson <tlwilliams92tech@yahoo.com>
2021-06-26 16:04:24 -0700

    working

--- app/src/UserForm.js
+++ app/src/UserForm.js
@@ -1,14 +1,32 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import  './App.css';
 
 
 function UserForm() {
+    const [data, setData] = useState({
+        username: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
+    });
+    const formHandler = event => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+    
     return (
         <div className="UserForm">
             <form>
-                <label>Username
-                <input type="text" />
+                <label> First Name:
+                    <input
+                        type="text" //type inputed
+                        value = {data.firstName}//data.
+                    name = 'First Name'
+                        onChange={formHandler}
+                   <input
+                    type=""
+                    />
                 </label>
                 </form>
         </div>
