b037c508d28b0881d6b16d4161c5cd27707528ba
Torquoya-Jackson <tlwilliams92tech@yahoo.com>
2021-06-28 03:44:43 -0700

    working in  userform state not holding

--- app/src/UserForm.js
+++ app/src/UserForm.js
@@ -1,71 +1,83 @@
+//import Axios from 'Axios';
+import React, { useState } from "react";
 
-import React, {useState} from 'react';
-import  './App.css';
+//import * as yup from 'yup'
 
+function UserForm(props) {
+  //setting the button state
+  // const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-function UserForm() {
-    // /const [isButtonDisabled, set]
-    const [data, setData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        username: '',
-        password: ''
-    });
-    const formHandler = event => {
-        setData({ ...data, [event.target.name]: event.target.value })
-    }
-    
-    return (
-        <div className="UserForm">
-            <form>
-                <label> First Name:
-                    <input
-                        type="text" //type inputed
-                        value = {data.firstName}//data.
-                    name = 'First Name'
-                        onChange={formHandler}
-                    />
-                </label>
-                <br></br>
-                   <label>Last Name:
-                    <input
-                    type="text"
-                    value = {data.lastName}
-                        name='Last Name'
-                        onChange={formHandler}
-                    />
-                </label>
-                <br></br>
-                     <label>Email:
-                    <input
-                    type="text"
-                    value = {data.email}
-                        name='Email'
-                        onChange={formHandler}
-                    />
-                </label>
-                <br></br>
-                <label> Username:
-                    <input
-                    type="text"
-                    value = {data.username}
-                        name='Username'
-                        onChange={formHandler}
-                    />
-                </label>
-                <br></br>
-                <label>Password:
-                    <input
-                    type="text"
-                    value = {data.password}
-                        name='Password'
-                        onChange={formHandler}
-                    />
-                </label>
-                </form>
-        </div>
-    )
+  // setting serber error state
+  //const [serverError, setServerError] = useState('');
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    username: "",
+    password: "",
+  });
+  const formHandler = (Event) => {
+    setData({ ...data, [Event.target.name]: Event.target.value });
+  };
+
+  return (
+    <div className="UserForm">
+      <form>
+        <label>
+          {" "}
+          First Name:
+          <input
+            type="text" //type inputed
+            value={data.firstName} //data.
+            name="First Name"
+            onChange={formHandler}
+          />
+        </label>
+        <br></br>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={data.lastName}
+            name="Last Name"
+            onChange={formHandler}
+          />
+        </label>
+        <br></br>
+        <label>
+          Email Address:
+          <input
+            type="text"
+            value={data.emailAddress}
+            name="Email Address"
+            onChange={formHandler}
+          />
+        </label>
+        <br></br>
+        <label>
+          {" "}
+          Username:
+          <input
+            type="text"
+            value={data.username}
+            name="Username"
+            onChange={formHandler}
+          />
+        </label>
+        <br></br>
+        <label>
+          Password:
+          <input
+            type="text"
+            value={data.password}
+            name="Password"
+            onChange={formHandler}
+          />
+        </label>
+      </form>
+    </div>
+  );
 }
 
 export default UserForm;
